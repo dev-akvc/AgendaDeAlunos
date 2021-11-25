51021eca8ccd46e3ae10e1048d20f0d56c462b00
Anna Karolyne <dev.akvc@gmail.com>
2021-11-24 22:29:45 -0300

    Criar toString para classe Aluno

--- app/src/main/java/com/example/agenda/model/Aluno.java
+++ app/src/main/java/com/example/agenda/model/Aluno.java
@@ -1,5 +1,7 @@
 package com.example.agenda.model;
 
+import androidx.annotation.NonNull;
+
 public class Aluno {
 
     private final String nome;
@@ -12,15 +14,10 @@ public class Aluno {
         this.email = email;
     }
 
-    public String getNome() {
+    @NonNull
+    @Override
+    public String toString() {
         return nome;
     }
 
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 }
